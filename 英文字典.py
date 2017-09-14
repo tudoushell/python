1080852fda3d1d98954ad09a97be285965379432
Binary <whitehatzk520@gmail.com>
2017-09-14 20:38:45 +0800

    search dictionary

---
+++ 英文字典.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  英文字典.py
+english_dictionary={ 
+	'position':"位置",
+	'chaos':"混乱",
+	'vain':"没有真正意义的",
+	'boligation':"义务",
+	'orbit':"轨道",
+	'bend':"弯曲",
+	'swallow':"吞下"
+	}
+dic=input("please type you want search word: ")
+if dic not in english_dictionary:
+	print("this word is not in dictionary,this word is add now")
+	dictionary_mean=input("plesase type this word mean: ")
+	english_dictionary[dic]=dictionary_mean
+	tip=input("is  look up  dictionary ? yes or no ")
+	if tip=="yes":
+		for i,j in english_dictionary.items():
+			print(i+":  "+j)
+	else:
+		print("Thank you")
+else:
+	print("this word mean is ")
+	print(english_dictionary[dic])
